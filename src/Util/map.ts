0fd79c4c81bc52ab6825bc124ff7a1393ed228ec
Thibaud REY <contact@thibaudrey.pro>
2018-08-09 11:57:50 +0200

    EngineObject prescisions

---
+++ src/Util/map.ts
@@ -0,0 +1,4 @@
+interface Map
+{
+    [key: string]: any;
+}
--- src/object.ts
+++ src/object.ts
@@ -1,16 +1,39 @@
 export class EngineObject {
+
+    // Position in scene on X-axis
     x: number;
+
+    // Position in scene on Y-Axis
     y: number;
+
+    // X-Axis Friction: speed loss at each frame
     cx: number;
+
+    // Y-Axis Friction: speed loss at each frame
     cy: number;
+
+    // Object mask width
     width: number;
+
+    // Object mask height
     height: number;
+
+    // Skin filepath
     skin: string;
+
+    // X-Axis speed
     vx: number;
+
+    // Y-Axis speed
     vy: number;
+
+    // X-Axis Inertia: distance allowed to move
     ix: number;
+
+    // Y-Axis Inertia: distance allowed to move
     iy: number;
 
+    // Unique identifier, needed to compute collisions
     hash: string;
 
     updateHash() {
